5a85fce5bba1a087f740dce2ac3995f6547f73c4
Michael Perkins <mperkins1995@gmail.com>
2023-09-06 03:12:49 -0500

    Add items to database functional

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { useEffect, useState } from 'react';
+import AddItemForm from './components/AddItemForm';
 
 function App() {
   const [items, setItems] = useState([]);
 
   useEffect(() => {
-    // Fetch items from your Flask API root route
     fetch(`${process.env.REACT_APP_API_URL}/`)
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
       .then((data) => setItems(data))
       .catch((error) => console.error('Error fetching data:', error));
   }, []);
 
   return (
     <div>
+      <AddItemForm />
       <h1>Items</h1>
       <ul>
         {items.map((item) => (
---
+++ src/components/AddItemForm.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+
+function AddItemForm() {
+  const [formData, setFormData] = useState({
+    name: '',
+    description: '',
+    price: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/add_item`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        // Item added successfully, update UI as needed
+        console.log('Item added successfully');
+      } else {
+        // Handle error here, display error message, etc.
+        console.error('Error adding item');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Add Item</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Name:</label>
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        {/* Add fields for description, price, etc. */}
+        <button type="submit">Add Item</button>
+      </form>
+    </div>
+  );
+}
+
+export default AddItemForm;
