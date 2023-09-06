9ea7581d3c9a216cfcc8e1b27b1686e539328568
Michael Perkins <mperkins1995@gmail.com>
2023-09-06 08:20:37 -0500

    Register connected to backend

---
+++ src/components/Register/Register.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+
+function Register() {
+  // State variables to manage user input
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  // Event handler to update form data as user types
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/auth/register`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        console.log('User registered successfully');
+      } else {
+        // Handle error here, display error message, etc.
+        console.error('Error regisdtering user');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+
+  return (
+    <div className="register-form">
+      <h2>Register</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
+
+export default Register;
