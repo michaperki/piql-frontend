d16695d26d96a3f21103e688f73602d1945f02bc
Michael Perkins <mperkins1995@gmail.com>
2023-09-06 23:00:17 -0500

    frontend routing

--- src/App.js
+++ src/App.js
@@ -1,39 +1,19 @@
 import './styles/tailwind.css'; // Import your Tailwind CSS file
 import React, { useEffect, useState } from 'react';
-import AddItemForm from './components/AddItemForm';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './components/Register';
 import Login from './components/Login'
 
+
 function App() {
-  const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    fetch(`${process.env.REACT_APP_API_URL}/`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then((data) => setItems(data))
-      .catch((error) => console.error('Error fetching data:', error));
-  }, []);
-
   return (
-    <div className="container mx-auto p-4">
-      <Register />
-      <Login />
-      <AddItemForm data-testid="addItemForm" />
-  
-      <h1 className="text-2xl font-bold mb-4">Items</h1>
-  
-      <ul className="list-disc pl-4">
-        {items.map((item) => (
-          <li key={item.id} className="text-lg">{item.name}</li>
-        ))}
-      </ul>
-    </div>
-  );  
+    <Router basename="/piql-frontend">
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
