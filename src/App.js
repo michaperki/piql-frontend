227ec3906f15ce1b09759e5d2af6385c614ca58b
Michael Perkins <mperkins1995@gmail.com>
2023-09-07 16:17:46 -0500

    add Navigation to app

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import { useNavigate } from 'react-router-dom';
+import Navigation from './components/Navigation';
 
 function App() {
   const navigate = useNavigate();
-  
+
   return (
     <>
       <h2>piql</h2>
+      <Navigation/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
