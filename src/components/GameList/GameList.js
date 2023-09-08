6eda6721725a7a3f236f0ada7186b2067a8fcb18
Michael Perkins <mperkins1995@gmail.com>
2023-09-07 23:29:30 -0500

    Adding GameList tests, all tests passing

--- src/components/GameList/GameList.js
+++ src/components/GameList/GameList.js
@@ -48,6 +48,7 @@ function GameList() {
               <p>Start Time: {game.start_time}</p>
               <p>End Time: {game.end_time}</p>
               <p>Court ID: {game.court_id}</p>
+              <p>Players: {game.player_ids.length > 1 ? game.player_ids.join(', ') : game.player_ids[0]}</p>
             </li>
           ))}
         </ul>
--- src/components/GameList/__tests__/GameList.test.js
+++ src/components/GameList/__tests__/GameList.test.js
@@ -1,10 +1,48 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import GameList from '../GameList'; // Adjust the import path as needed
-import '@testing-library/jest-dom'
+import { render, waitFor, screen } from '@testing-library/react';
+import GameList from '../GameList';
+import '@testing-library/jest-dom/extend-expect'; // Import jest-dom to extend expect function
 
-test('renders the CreateGame', () => {
-  render(<GameList />);
-  const text = screen.getAllByText('Game List'); // Use getAllByText to handle multiple matches
-  expect(text.length).toBeGreaterThan(0); // Check that at least one element with text "Add Item" is found
+// Mock the fetch function to return a sample game list
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () =>
+      Promise.resolve([
+        {
+          id: 1,
+          date: '2023-09-10',
+          start_time: '14:30:00',
+          end_time: '16:30:00',
+          court_id: 1,
+          player_ids: [6],
+        },
+        // Add more sample game data as needed
+      ]),
+    ok: true,
+  })
+);
+
+describe('GameList', () => {
+  it('should render loading message initially', async () => {
+    render(<GameList />);
+    const loadingText = screen.getByText('Loading...');
+    expect(loadingText).toBeInTheDocument();
+  });
+
+  it('should render game data after fetching', async () => {
+    render(<GameList />);
+    await waitFor(() => {
+      const dateText = screen.getByText('Date: 2023-09-10');
+      const startTimeText = screen.getByText('Start Time: 14:30:00');
+      const endTimeText = screen.getByText('End Time: 16:30:00');
+      const courtIdText = screen.getByText('Court ID: 1');
+      const playersText = screen.getByText('Players: 6');
+      
+      expect(dateText).toBeInTheDocument();
+      expect(startTimeText).toBeInTheDocument();
+      expect(endTimeText).toBeInTheDocument();
+      expect(courtIdText).toBeInTheDocument();
+      expect(playersText).toBeInTheDocument();
+    });
+  });
 });
