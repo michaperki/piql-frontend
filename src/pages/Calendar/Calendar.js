61ee0482bd1fe3c2b7a0106da5b1ee3f46fe3076
Michael Perkins <mperkins1995@gmail.com>
2023-09-09 02:22:40 -0500

    Calendar displays games

--- src/pages/Calendar/Calendar.js
+++ src/pages/Calendar/Calendar.js
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-const Calendar = () => {
+function Calendar() {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    // Fetch games for the logged-in user
+    const accessToken = localStorage.getItem("access_token");
+    const headers = {
+      Authorization: `Bearer ${accessToken}`,
+    };
+
+    fetch(`${process.env.REACT_APP_API_URL}/api/games/user`, {
+      method: "GET",
+      headers,
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch games");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setGames(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching games:", error);
+      });
+  }, []);
+
+  // Render your calendar with the fetched games
   return (
-    <div>
-      <h1>Calendar</h1>
-      {/* Add your calendar content here */}
+    <div className="calendar">
+      {/* Render your calendar here */}
+      {games.map((game) => (
+        <div key={game.id} className="calendar-event">
+          <span>{game.date}</span>
+          <span>{game.start_time} - {game.end_time}</span>
+          <span>Court {game.court_id}</span>
+        </div>
+      ))}
     </div>
   );
-};
+}
 
 export default Calendar;
